ea6bae4108f54d2e3d92b8bef12210345abc990c
Andy f <aforbes@knights.ucf.edu>
2018-05-02 11:00:45 -0400

    pushing to help with maximum call error

---
+++ app/data/questions.js
@@ -0,0 +1,3 @@
+var questions = ["I am always perpared and follow a schedule", "I do not have a active imagination", "I feel comfortable talking to large crowds and meeting new people", "I am interested in helping people's problems" , "I get irritated and stressed easily" , "I enjoy using difficult words" , "I never leave my belongs laying around" , "I like drawing attention to myself" , "I make people feel at ease" , "I am much more anxious than most people"];
+
+module.exports = questions;
